a34ff4bef1d051148019e1fdb56e5fc4ff1a437e
Marc Plunkett <marc@Marcs-iMac.lan>
2020-07-26 00:01:29 +0100

    DOne DFT graphs up to H2S

--- Chapter3/data/analysis.py
+++ Chapter3/data/analysis.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import make_interp_spline, BSpline
 from random import random
 
+## H2
+
 df = pd.read_csv(
     '/Users/marc/Thesis/Chapter3/data/Hydrogen_adsorption.csv')
 
@@ -40,8 +42,6 @@ Pd70Cu20Zr10 = df[ df['Metal'] == 'Pd70Cu20Zr10']
 Pd70Zr20Ag10 = df[ df['Metal'] == 'Pd70Zr20Ag10']
 
 
-
-
 PD
 
 PDAu10
@@ -55,7 +55,7 @@ plt.style.use('science')
 
 
 with plt.style.context(['science']):
-    fig, ax = plt.subplots(figsize=(8,3))
+    fig, ax = plt.subplots(figsize=(8,8))
     ax.plot(PD['Site'], PD['adsry'], label='Pd', linewidth=2)
     ax.plot(PDAu10['Site'], PDAu10['adsry'], label='PdAu10', linewidth=2)
     ax.plot(PdAu20['Site'], PdAu20['adsry'], label='PdAu20', linewidth=2)
@@ -72,10 +72,10 @@ with plt.style.context(['science']):
     plt.legend(bbox_to_anchor=(1, 1),
     bbox_transform=plt.gcf().transFigure)
     fig.show()
-    fig.savefig('h2.jpg', dpi=300)
+    fig.savefig('h2_1.jpg', dpi=300)
 
 with plt.style.context(['science']):
-    fig, ax = plt.subplots(figsize=(8,3))
+    fig, ax = plt.subplots(figsize=(8,8))
     ax.plot(PD['Site'], PD['adsry'], label='Pd', linewidth=2)
     
     ax.plot(Pd70Au20Ag10['Site'], Pd70Au20Ag10['adsry'], label='Pd70Au20Ag10', linewidth=2)
@@ -93,9 +93,194 @@ with plt.style.context(['science']):
     plt.legend(bbox_to_anchor=(1, 1),
     bbox_transform=plt.gcf().transFigure)
     fig.show()
-    fig.savefig('h2.jpg', dpi=300)
+    fig.savefig('h2_2.jpg', dpi=300)
+
+#######################################################################################################
+## CO #################################################################################################
+#######################################################################################################
+def CO_graphs():
+
+    df = pd.read_csv(
+        '/Users/marc/Thesis/Chapter3/data/CO_adsorption.csv')
+
+    df
+
+    df['adsry'] = df['Adsorption energy(kJ)'] /(2.179872*10**(-21))
+
+    isPd = df['Metal'] == 'Pd'
+    isPdAu10 = df['Metal'] == 'PdAu10'
+    isPdAg23 = df['Metal'] == 'PdAg23'
+    isPd80Cu20 = df['Metal'] == 'Pd80Cu20'
+    isPd60Cu40 = df['Metal'] == 'Pd60Cu40'
+    isPdAu20 = df['Metal'] == 'PdAu20'
+    isPdZr10 = df['Metal'] == 'PdZr10'
+    isPdZr20 = df['Metal'] == 'PdZr20'
+    isPd70Au20Ag10 = df['Metal'] == 'Pd70Au20Ag10'
 
 
+    PD = df[isPd]
+    PDAu10 = df[isPdAu10]
+    PdAg23 = df[isPdAg23]
+    Pd80Cu20 = df[isPd80Cu20]
+    Pd60Cu40 = df[isPd60Cu40]
+    PdAu20 = df[isPdAu20]
+    PdZr10 = df[isPdZr10]
+    PdZr20 = df[isPdZr20]
+    Pd70Au20Ag10 = df[isPd70Au20Ag10]
+    Pd70Au20Cu10 = df[ df['Metal'] == 'Pd70Au20Cu10']
+    Pd70Au20Zr10 = df[ df['Metal'] == 'Pd70Au20Zr10']
+    Pd70Cu20Ag10 = df[ df['Metal'] == 'Pd70Cu20Ag10']
+    Pd70Au20Zr10 = df[ df['Metal'] == 'Pd70Au20Zr10']
+    Pd70Cu20Zr10 = df[ df['Metal'] == 'Pd70Cu20Zr10']
+    Pd70Zr20Ag10 = df[ df['Metal'] == 'Pd70Zr20Ag10']
+
+
+    PD
+
+    PDAu10
+
+    # spl = make_interp_spline(x, data, k=3)  # type: BSpline
+    # power_smooth = spl(x_new)
+
+
+
+    plt.style.use('science')
+
+
+    with plt.style.context(['science']):
+        fig, ax = plt.subplots(figsize=(8,8))
+        ax.plot(PD['Site'], PD['adsry'], label='Pd', linewidth=2)
+        ax.plot(PDAu10['Site'], PDAu10['adsry'], label='PdAu10', linewidth=2)
+        ax.plot(PdAu20['Site'], PdAu20['adsry'], label='PdAu20', linewidth=2)
+        ax.plot(PdAg23['Site'], PdAg23['adsry'], label='PdAg23', linewidth=2)
+        ax.plot(Pd80Cu20['Site'], Pd80Cu20['adsry'], label='Pd80Cu20', linewidth=2)
+        ax.plot(Pd60Cu40['Site'], Pd60Cu40['adsry'], label='Pd60Cu40', linewidth=2)
+        ax.plot(PdZr10['Site'], PdZr10['adsry'], label='PdZr10', linewidth=2)
+        ax.plot(PdZr20['Site'], PdZr20['adsry'], label='PdZr20', linewidth=2)
+
+        ax.set(xlabel="Adsorption site")
+        ax.set(ylabel="Adsorption energy (Ry)")
+        fig.suptitle('CO adsorption (Binary)', fontsize=10)
+        ax.autoscale(tight=False)
+        plt.legend(bbox_to_anchor=(1, 1),
+        bbox_transform=plt.gcf().transFigure)
+        fig.show()
+        fig.savefig('CO_1.jpg', dpi=300)
+
+    with plt.style.context(['science']):
+        fig, ax = plt.subplots(figsize=(8,8))
+        ax.plot(PD['Site'], PD['adsry'], label='Pd', linewidth=2)
+    
+        ax.plot(Pd70Au20Ag10['Site'], Pd70Au20Ag10['adsry'], label='Pd70Au20Ag10', linewidth=2)
+        ax.plot(Pd70Au20Cu10['Site'], Pd70Au20Cu10['adsry'], label='Pd70Au20Cu10', linewidth=2)
+        ax.plot(Pd70Au20Zr10['Site'], Pd70Au20Zr10['adsry'], label='Pd70Au20Zr10', linewidth=2)
+        ax.plot(Pd70Cu20Ag10['Site'], Pd70Cu20Ag10['adsry'], label='Pd70Cu20Ag10', linewidth=2)
+        ax.plot(Pd70Au20Zr10['Site'], Pd70Au20Zr10['adsry'], label='Pd70Au20Zr10', linewidth=2)
+        ax.plot(Pd70Cu20Zr10['Site'], Pd70Cu20Zr10['adsry'], label='Pd70Cu20Zr10', linewidth=2)
+        ax.plot(Pd70Zr20Ag10['Site'], Pd70Zr20Ag10['adsry'], label='Pd70Zr20Ag10', linewidth=2)
+
+        ax.set(xlabel="Adsorption site")
+        ax.set(ylabel="Adsorption energy (Ry)")
+        fig.suptitle('CO adsorption (Ternary)', fontsize=10)
+        ax.autoscale(tight=False)
+        plt.legend(bbox_to_anchor=(1, 1),
+        bbox_transform=plt.gcf().transFigure)
+        fig.show()
+        fig.savefig('CO_2.jpg', dpi=300)
+
+CO_graphs()
+
+def H2S_graphs():
+
+    df = pd.read_csv(
+        '/Users/marc/Thesis/Chapter3/data/h2S_adsorption.csv')
+
+    df
+
+    df['adsry'] = df['Adsorption energy(kJ)'] /(2.179872*10**(-21))
+
+    isPd = df['Metal'] == 'Pd'
+    isPdAu10 = df['Metal'] == 'PdAu10'
+    isPdAg23 = df['Metal'] == 'PdAg23'
+    isPd80Cu20 = df['Metal'] == 'Pd80Cu20'
+    isPd60Cu40 = df['Metal'] == 'Pd60Cu40'
+    isPdAu20 = df['Metal'] == 'PdAu20'
+    isPdZr10 = df['Metal'] == 'PdZr10'
+    isPdZr20 = df['Metal'] == 'PdZr20'
+    isPd70Au20Ag10 = df['Metal'] == 'Pd70Au20Ag10'
+
+
+    PD = df[isPd]
+    PDAu10 = df[isPdAu10]
+    PdAg23 = df[isPdAg23]
+    Pd80Cu20 = df[isPd80Cu20]
+    Pd60Cu40 = df[isPd60Cu40]
+    PdAu20 = df[isPdAu20]
+    PdZr10 = df[isPdZr10]
+    PdZr20 = df[isPdZr20]
+    Pd70Au20Ag10 = df[isPd70Au20Ag10]
+    Pd70Au20Cu10 = df[ df['Metal'] == 'Pd70Au20Cu10']
+    Pd70Au20Zr10 = df[ df['Metal'] == 'Pd70Au20Zr10']
+    Pd70Cu20Ag10 = df[ df['Metal'] == 'Pd70Cu20Ag10']
+    Pd70Au20Zr10 = df[ df['Metal'] == 'Pd70Au20Zr10']
+    Pd70Cu20Zr10 = df[ df['Metal'] == 'Pd70Cu20Zr10']
+    Pd70Zr20Ag10 = df[ df['Metal'] == 'Pd70Zr20Ag10']
+
+
+    PD
+
+    PDAu10
+
+    # spl = make_interp_spline(x, data, k=3)  # type: BSpline
+    # power_smooth = spl(x_new)
+
+
+
+    plt.style.use('science')
+
+
+    with plt.style.context(['science']):
+        fig, ax = plt.subplots(figsize=(8,8))
+        ax.plot(PD['Site'], PD['adsry'], label='Pd', linewidth=2)
+        ax.plot(PDAu10['Site'], PDAu10['adsry'], label='PdAu10', linewidth=2)
+        ax.plot(PdAu20['Site'], PdAu20['adsry'], label='PdAu20', linewidth=2)
+        ax.plot(PdAg23['Site'], PdAg23['adsry'], label='PdAg23', linewidth=2)
+        ax.plot(Pd80Cu20['Site'], Pd80Cu20['adsry'], label='Pd80Cu20', linewidth=2)
+        ax.plot(Pd60Cu40['Site'], Pd60Cu40['adsry'], label='Pd60Cu40', linewidth=2)
+        ax.plot(PdZr10['Site'], PdZr10['adsry'], label='PdZr10', linewidth=2)
+        ax.plot(PdZr20['Site'], PdZr20['adsry'], label='PdZr20', linewidth=2)
+
+        ax.set(xlabel="Adsorption site")
+        ax.set(ylabel="Adsorption energy (Ry)")
+        fig.suptitle('CO adsorption (Binary)', fontsize=10)
+        ax.autoscale(tight=False)
+        plt.legend(bbox_to_anchor=(1, 1),
+        bbox_transform=plt.gcf().transFigure)
+        fig.show()
+        fig.savefig('CO_1.jpg', dpi=300)
+
+    with plt.style.context(['science']):
+        fig, ax = plt.subplots(figsize=(8,8))
+        ax.plot(PD['Site'], PD['adsry'], label='Pd', linewidth=2)
+    
+        ax.plot(Pd70Au20Ag10['Site'], Pd70Au20Ag10['adsry'], label='Pd70Au20Ag10', linewidth=2)
+        ax.plot(Pd70Au20Cu10['Site'], Pd70Au20Cu10['adsry'], label='Pd70Au20Cu10', linewidth=2)
+        ax.plot(Pd70Au20Zr10['Site'], Pd70Au20Zr10['adsry'], label='Pd70Au20Zr10', linewidth=2)
+        ax.plot(Pd70Cu20Ag10['Site'], Pd70Cu20Ag10['adsry'], label='Pd70Cu20Ag10', linewidth=2)
+        ax.plot(Pd70Au20Zr10['Site'], Pd70Au20Zr10['adsry'], label='Pd70Au20Zr10', linewidth=2)
+        ax.plot(Pd70Cu20Zr10['Site'], Pd70Cu20Zr10['adsry'], label='Pd70Cu20Zr10', linewidth=2)
+        ax.plot(Pd70Zr20Ag10['Site'], Pd70Zr20Ag10['adsry'], label='Pd70Zr20Ag10', linewidth=2)
+
+        ax.set(xlabel="Adsorption site")
+        ax.set(ylabel="Adsorption energy (Ry)")
+        fig.suptitle('CO adsorption (Ternary)', fontsize=10)
+        ax.autoscale(tight=False)
+        plt.legend(bbox_to_anchor=(1, 1),
+        bbox_transform=plt.gcf().transFigure)
+        fig.show()
+        fig.savefig('CO_2.jpg', dpi=300)
+
+H2S_graphs()
 
 # DN34
 # B543
